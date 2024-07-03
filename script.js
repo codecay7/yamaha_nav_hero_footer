b388297b087deceaced6837d5140974b58b9bfe5
Diwakar  Kumar <110222935+codecay7@users.noreply.github.com>
2024-07-03 12:56:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+gsap.from(".box", {
+    opacity: 0,
+    // x: "50%",
+    y: "500px",
+    duration: 0.7,
+    delay: 0.5,
+    // repeat: -1,
+    yoyo: true,
+    stagger:1
+
+})
+gsap.from(".box h2", {
+    opacity: 0,
+    // x: "50%",
+    y: "700px",
+    duration: 0.7,
+    delay: 0.5,
+    // repeat: -1,
+    yoyo: true
+,stagger:1
+})
+
+gsap.from(".videosection video", {
+    opacity: 0,
+    // x: "50%",
+    // y: "700px",
+    duration: 3,
+    // delay: 1,
+    // repeat: -1,
+    yoyo: true
+
+})
+gsap.from(".tagline h1", {
+    opacity: 0,
+   
+    y: "700px",
+    duration: 1,
+    delay: 7,
+    // repeat: -1,
+    yoyo: true
+
+})
+
+
+
+
